e51df44dbd342ae0eef87815a1a293db0b1ff175
deshabee <mostafafcis15@gmail.com>
2019-02-06 02:47:49 +0200

    Home page done

--- wp-content/themes/wohr/index.php
+++ wp-content/themes/wohr/index.php
@@ -7,16 +7,20 @@
             <div class="ce_gallery first last block">
               <div class="flex-container" style="display:none;">
                 <div id="fluid-slider">
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/d/Torre-Reforma-WOeHR-fullyautomaticparkingsystem-9392e7fd.jpg" width="1200"
-                    height="1200" alt="">
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/e/Woehr-vollautomatisches-parksystem-Torre-Reforma-Mexico-0416767e.jpg" width="1200"
-                    height="900" alt="">
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/e/Bueroturm%20Torre-Reforma-Mexiko-Stadt-bd6fba5e.jpg" width="1200" height="797"
-                    alt="">
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/a/Uebergabekabinen%20WOeHR%20Multiparker%20750-ae84faea.jpg" width="1200"
-                    height="833" alt="">
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/3/Uebergabebereich-fullyautomatic-parking-system-Woehr-torre-reforma-7b75be33.jpg"
-                    width="1200" height="798" alt="">
+                  <?php
+                      $args = array(
+                        'category_name'    => 'home-slider',
+                        'post_type'        => 'post',
+                        'post_status'      => 'publish',
+                      );
+                      $postArray = get_posts( $args );
+                      foreach ($postArray as $post) {
+                        $image = get_the_post_thumbnail_url($post->ID);
+                  ?>
+                  <img src="<?php echo $image;  ?>" class="home-slider-img" alt="">
+                 <?php
+                  }
+                 ?>
                 </div>
                 <div id="teaser-container">
                   <div class="teaser-inner">
@@ -29,117 +33,66 @@
                         </div>
                         <div class="text">
                           <div class="inner">
-                            <h1>WÖHR Autoparksysteme GmbH – One of the leading manufacturers of automatic parking
-                              systems in Europe</h1>
-                            <p>WÖHR Autoparksysteme GmbH is one of the leading manufacturers of automatic parking
-                              systems. We develop, produce and install parking space solutions. Our 14 product series
-                              provide us with the largest product range worldwide. Thanks to both mechanical and semi
-                              and fully automatic parking systems, we provide a multitude of parking place solutions.</p>
-                            <p>The Parklift series is one of our mechanical parking systems. Here, vehicles can be
-                              accommodated one above the other on two or three parking levels. In addition, double
-                              units can be installed for up to six parking spaces. Parking spaces are made available
-                              using mechanically useable parking platforms - which can be rotated or shifted crossways
-                              and lengthways - and turntables made by Wöhr.</p>
-                            <p>By stacking and closing ranks, semi-automatic parking place solutions, such as Combilift
-                              and Combiparker, enable parking spaces for up to 31 multi-sized vehicles per parking
-                              place to be set up in one parking system. Slimparker and Crossparker operate on the same
-                              principle. These systems are located underground. As a result, the sunken entrance on the
-                              plot surface remains free for vehicles.</p>
-                            <p>Fully automatic parking systems make possible up-to-date problem solutions and compact
-                              parking in city centres. In fully automatic systems, such as the Levelparker, Parksafe
-                              and Multiparker the "Parking place comes to the driver" - no need then to go looking for
-                              the car. The hand-over stations make it easy to get in and out and save the user the
-                              trouble of undertaking complex manoeuvring. A vertical lift transfers the vehicles on
-                              parking platforms to the shelf system. As such, they are securely parked and remain
-                              untouched. Depending on the surface involved, these solutions permit several hundred
-                              vehicles to be parked in towers or high-rack systems on different parking levels.</p>
-                            <p>The automatic Bikesafe parking system stores bikes and E-bikes both securely and
-                              compactly in a tower.</p>
-                            <p>All Stackparkers and Liftparkers can be used in single, pre-fabricated, underground and
-                              surface garages as well as in garage systems.</p>
+                            <?php 
+                                $args = array(
+                                  'category_name'    => 'info-homepage',
+                                  'post_type'        => 'post',
+                                  'post_status'      => 'publish',
+                                );
+                                $infopost = get_posts( $args );
+                            ?>
+                            <h1><?php echo $infopost[0]->post_title; ?></h1>
+                            <?php echo $infopost[0]->post_content; ?>
                           </div>
                         </div>
                       </div>
                       <!-- indexer::stop -->
                       <div class="mod_newslist block">
-                        <a href="en/news-/international-highrise-award-2018.html" class="news_latest block first last even"
-                          data-link="en/news-/international-highrise-award-2018.html">
-                          <div class="image">
-                            <img src="<?php echo get_template_directory_uri(); ?>/tl_files/uploads/woehr/start/Torre-Reforma-Mexico-City.jpg" width="2894" height="5210"
+                      <?php 
+                                $args = array(
+                                  'category_name'    => 'bottom-homepage',
+                                  'post_type'        => 'post',
+                                  'post_status'      => 'publish',
+                                );
+                                $bottompost = get_posts( $args );
+                                $image = get_the_post_thumbnail_url($bottompost[0]->ID); 
+                            ?>
+                        <a href="<?php echo $bottompost[0]->guid; ?>" class="news_latest block first last even"
+                          data-link="<?php echo $bottompost[0]->guid; ?>">
+                          <div class="image bottom-home">
+                            <img style="width:100% !important" src="<?php echo $image; ?>" width="2894" height="5210"
                               alt="Highest Building Torre Reforma in Mexico City">
                           </div>
                           <div class="text">
-                            <h4>INTERNATIONAL HIGHRISE AWARD 2018</h4>
-                            <p>The world´s most innovative skyscraper -Torre Reforma in Mexico-City - relies on WÖHR!</p>
+                            <h4><?php echo $bottompost[0]->post_title; ?></h4>
+                            <p><?php echo $bottompost[0]->post_excerpt; ?></p>
                           </div>
                         </a>
                       </div>
                       <!-- indexer::continue -->
                     </div>
-                    <div class="right-box" hidden="">
-                      <div class="right-box-top">
-                        <div class="ce_image block" id="banner_wt">
-                          <figure class="image_container">
-                            <a href="http://bauwelt.woehr.de/en">
-                              <img src="<?php echo get_template_directory_uri(); ?>/tl_files/uploads/woehr/banner/parking3_en.png" width="300" height="115" alt="Parking3 - Slim Parking - Urbane Mobilität">
-                            </a>
-                            <figcaption class="caption">Parking3 - Slim Parking - Urbane Mobilität</figcaption>
-                          </figure>
-                        </div>
-                      </div>
-                      <div class="right-box-bottom">
-                        <div class="ce_image block" id="banner_wt">
-                          <figure class="image_container">
-                            <a href="en/tickets.html">
-                              <img src="<?php echo get_template_directory_uri(); ?>/tl_files/uploads/woehr/banner/banner_englisch.jpg" width="610" height="230" alt="">
-                            </a>
-                          </figure>
-                        </div>
-                      </div>
-                    </div>
+
                   </div>
                 </div>
                 <div id="fluid-captions">
                   <div class="controlnav">
                     <ul>
+                    <?php 
+                    foreach ($postArray as $post) {
+                      ?>
                       <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
+                    <?php } ?>
                     </ul>
                   </div>
                   <div class="slides">
+                  <?php 
+                    foreach ($postArray as $post) {
+                  ?>                    
                     <div class="slide">
-                      <p>The office tower Torre Reforma in Mexico-City has been awarded with the International Highrise
-                        Award 2018. WÖHR has installed 424 parking spaces with the fully automatic parking system
-                        Multiparker 750.</p>
+                      <p><?php echo $post->post_content; ?></p>
                       <p class="headline"></p>
                     </div>
-                    <div class="slide">
-                      <p>The office tower Torre Reforma in Mexico-City has been awarded with the International Highrise
-                        Award 2018. WÖHR has installed 424 parking spaces with the fully automatic parking system
-                        Multiparker 750.</p>
-                      <p class="headline"></p>
-                    </div>
-                    <div class="slide">
-                      <p>The office tower Torre Reforma in Mexico-City has been awarded with the International Highrise
-                        Award 2018. WÖHR has installed 424 parking spaces with the fully automatic parking system
-                        Multiparker 750.</p>
-                      <p class="headline"></p>
-                    </div>
-                    <div class="slide">
-                      <p>The office tower Torre Reforma in Mexico-City has been awarded with the International Highrise
-                        Award 2018. WÖHR has installed 424 parking spaces with the fully automatic parking system
-                        Multiparker 750.</p>
-                      <p class="headline"></p>
-                    </div>
-                    <div class="slide">
-                      <p>The office tower Torre Reforma in Mexico-City has been awarded with the International Highrise
-                        Award 2018. WÖHR has installed 424 parking spaces with the fully automatic parking system
-                        Multiparker 750.</p>
-                      <p class="headline"></p>
-                    </div>
+                    <?php } ?>
                   </div>
                 </div>
               </div>
